4afa291b9cc2864c0fe968410ae7f2e0171d4a23
efraimpolla <efraimpolla@hotmail.com>
2016-02-28 23:32:23 -0300

    Adicionado comentarios e verificações

--- app.js
+++ app.js
@@ -15,13 +15,22 @@ var tamanho = 0;
 
 var adicionaNaAgenda = function()
 {
+	//recupera valores
 	var qtd = $("#qtd").val();
 	var numeros = $("#numeros").val().trim();
+	//separa os numeros em um novo vetor
 	var vet = numeros.split(",");
 	
+	if (!qtd || qtd != vet.length)
+	{
+		alert("Quantidade de números é diferente da quantidade inserida.");
+		return;
+	}
+	// ordena o vetor 
+	//trata erros quando insere 0123,012,432,01245 por exemplo
 	vet.sort();
-	tamanho = vet[0].length;
 	
+	tamanho = vet[0].length;
 	var raiz = vet[0];
 	for (var i = 1; i < qtd; i++)
 	{
@@ -35,6 +44,7 @@ var adicionaNaAgenda = function()
 	
 	alert("Quantidade usada: " + tamanho);
 }
+//calcula quanto de tamanho será necessario
 function soma (raiz,temp)
 {
 	for (var j = 0; j <= temp.length;j++)
